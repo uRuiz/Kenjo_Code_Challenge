276cea1f5375c247058abbc2f34db84b590a39dc
uRuiz <zaszur@gmail.com>
2020-08-13 00:13:42 +0200

    Added artist list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { SearchComponent } from './components/search/search.component';
 import { AlbumComponent } from './components/album/album.component';
 import { ArtistComponent } from './components/artist/artist.component';
+import { ArtistsListComponent } from './components/artists-list/artists-list.component';
 
 export const ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'search', component: SearchComponent },
   { path: 'album/:id', component: AlbumComponent },
   { path: 'artist/:id', component: ArtistComponent },
-  { path: 'artists', component: ArtistComponent },
+  { path: 'artists', component: ArtistsListComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
